1bde5f799d99912909f8b71cc3829d7760cd18a2
huangxulei <pjhxl@qq.com>
2025-11-06 22:30:10 +0800

    Update .gitignore and remove .next from tracking

--- .next/dev/static/chunks/3c2ab_@tauri-apps_api_window_4ed10ec8.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/3c2ab_@tauri-apps_api_b7bf18a5._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_layout_tsx_11117d05._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_services_nativeAppService_ts_8f8140ae._.js",
-  "static/chunks/_c4084afb._.js",
-  "static/chunks/src_styles_globals_47180df9.css"
-],
-    source: "dynamic"
-});
